0c60437cd9122d150f7848ca25fc878efa16c2d5
emre@rakam.io <emre@rakam.io>
2021-04-04 20:44:25 +0300

    changed models/sources.yml

--- .dbt/models/source__tpch__orders__segmentation__total_orders_priority.sql
+++ .dbt/models/source__tpch__orders__segmentation__total_orders_priority.sql
@@ -1,12 +1,13 @@
 {{
   config(
    
+    schema = "EVENTS",
     alias = "SOURCE__TPCH__ORDERS__TOTAL_ORDERS_PRIORITY",
-    materialized = "table"
+    materialized = "incremental"
   )
 }}
-SELECT 
-    source__tpch__orders.o_orderdate AS o_orderdate,
+SELECT * FROM (SELECT 
+    CONVERT_TIMEZONE('UTC', source__tpch__orders.o_orderdate) AS o_orderdate,
     source__tpch__orders.o_shippriority AS o_shippriority,
     source__tpch__orders.o_orderpriority AS o_orderpriority,
     count(1) AS count_of_rows,
@@ -17,3 +18,7 @@ FROM SNOWFLAKE_SAMPLE_DATA.TPCH_SF1.ORDERS AS source__tpch__orders
 GROUP BY
     1,  2,  3 
 
+) AS t
+                            {% if is_incremental() %}
+                               WHERE source__tpch__orders.o_orderdate > (select max(o_orderdate) from {{ this }})
+                            {% endif %}
