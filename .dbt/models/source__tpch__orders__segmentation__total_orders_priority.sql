6939ddc9f4948dcb8385856ecaef0d96ef26dfbc
emre@rakam.io <emre@rakam.io>
2021-04-06 15:36:49 +0000

    changed models/sources.yml

--- .dbt/models/source__tpch__orders__segmentation__total_orders_priority.sql
+++ .dbt/models/source__tpch__orders__segmentation__total_orders_priority.sql
@@ -10,7 +10,7 @@ SELECT * FROM (SELECT
     CONVERT_TIMEZONE('UTC', source__tpch__orders.o_orderdate) AS o_orderdate,
     source__tpch__orders.o_shippriority AS o_shippriority,
     source__tpch__orders.o_orderpriority AS o_orderpriority,
-    count(1) AS count_of_rows,
+    count(*) AS count_of_rows,
     hll_accumulate(source__tpch__orders.o_orderkey) AS unique_orders
 FROM SNOWFLAKE_SAMPLE_DATA.TPCH_SF1.ORDERS AS source__tpch__orders
 
